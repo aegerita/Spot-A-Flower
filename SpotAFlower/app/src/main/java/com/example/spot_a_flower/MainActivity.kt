ce38d2b38ef5394b8fa770bc102c3fa857558728
aegerita <jennytai3221@gmail.com>
2020-03-17 16:46:16 -0400

    ask for permission for camera able to store the taken photo

--- SpotAFlower/app/src/main/java/com/example/spot_a_flower/MainActivity.kt
+++ SpotAFlower/app/src/main/java/com/example/spot_a_flower/MainActivity.kt
@@ -1,12 +1,82 @@
 package com.example.spot_a_flower
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val CAMERA_REQUEST = 1
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // declare the button to take photo
+        val cameraButton = findViewById<Button>(R.id.cameraButton)
+
+        // ask for permission if haven't got one
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST
+            )
+        }
+
+        // take photo when button is clicked
+        cameraButton.setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                takePictureIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
     }
+
+    // if the permission is not granted
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_REQUEST -> {
+                if ((grantResults.isEmpty() || grantResults[0] == PackageManager.PERMISSION_DENIED)) {
+                    //cameraButton.isEnabled = false
+                    cameraButton.setOnClickListener {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Pls Allow Camera Access",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+        return
+    }
+
+    // save the photo after it's taken
+    // for now just replace the logo, later pass it to the neural network
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val extras = data?.extras
+            val imageBitmap = extras!!["data"] as Bitmap?
+            logo.setImageBitmap(imageBitmap)
+        }
+    }
+
 }
