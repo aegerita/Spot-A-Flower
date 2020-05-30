d7e676bb96d2abceeb8e462ddf2532ff88de689b
Jenny <56517153+aegerita@users.noreply.github.com>
2020-05-30 03:28:31 -0400

    Neural network at 48% accuracy!! can now plot accuracy graph

--- FlowerML/FlowerML.py
+++ FlowerML/FlowerML.py
@@ -2,11 +2,13 @@ import os
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from tensorflow.python.ops.image_ops_impl import ResizeMethod
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-image_size = 128
+image_size = 32
+batch_size = 64
 
 ds_train, ds_test, ds_validation = tfds.load(
     'oxford_flowers102',
@@ -28,32 +30,33 @@ ds_train = ds_train.map(
     normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 ds_train = ds_train.cache()
 ds_train = ds_train.shuffle(1000)
-ds_train = ds_train.batch(64)
+ds_train = ds_train.batch(batch_size)
 ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
 ds_test = ds_test.map(
     normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-ds_test = ds_test.batch(64)
+ds_test = ds_test.batch(batch_size)
 ds_test = ds_test.cache()
 ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
 
 ds_validation = ds_validation.map(
     normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-ds_validation = ds_validation.batch(64)
+ds_validation = ds_validation.batch(batch_size)
 ds_validation = ds_validation.cache()
 ds_validation = ds_validation.prefetch(tf.data.experimental.AUTOTUNE)
 
 model = tf.keras.models.Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(image_size, image_size, 3)),
+    Conv2D(32, (2, 2), activation='relu', input_shape=(image_size, image_size, 3)),
     MaxPooling2D(2, 2),
-    Dropout(0.2),
+    Dropout(0.35),
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D(2, 2),
-    Conv2D(64, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
-    Dropout(0.2),
+    Dropout(0.35),
+    Conv2D(64, (3, 3), use_bias=False, strides=2, activation='relu'),
+    Dropout(0.25),
     Flatten(),
     Dense(512, activation='relu'),
+    Dropout(0.5),
     Dense(112, activation='softmax'),
 ])
 model.summary()
@@ -64,12 +67,19 @@ model.compile(
     metrics=['accuracy'],
 )
 
-model.fit(
+history = model.fit(
     ds_test,
-    epochs=10,
+    epochs=30,
     validation_data=ds_validation,
 )
 
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 
 test_accuracy = tf.keras.metrics.Accuracy()
 for (x, y) in ds_train:
@@ -92,6 +102,6 @@ predictions = model(test_pic, training=False)
 print(predictions)
 """
 
-if test_accuracy.result() > 0.33:
+if test_accuracy.result() > 0.48:
     model.save('flower_model')
     print('saved')
